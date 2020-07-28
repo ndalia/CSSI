bd8103a562f05ef77361ddf652ab2f379682e36a
Sneha  Jog <sneha@Snehas-MacBook-Pro.local>
2020-07-28 13:43:13 -0700

    re attached js to json

--- notesDisplay.js
+++ notesDisplay.js
@@ -17,8 +17,4 @@ document.addEventListener('DOMContentLoaded', function () {
     
   }, false)
 
-  chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  });
+ 
