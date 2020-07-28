4d79dc2ebba1b6f639e56d26f92b510f0655a19c
ndalia <nehadalia@hotmail.com>
2020-07-28 14:24:26 -0700

    notes works

--- notes.js
+++ notes.js
@@ -1,56 +1,124 @@
-zIndex = 10;
-$('#new').click(function(){
-    $('#notes')
-        .append('\
-            <div class="sticky-note-pre ui-widget-content">\
-                <div class="handle">&nbsp;<div class="close">&times;</div></div>\
-                <div contenteditable class="contents">awesome</div>\
-            </div>\
-         ')
-        .find('.sticky-note-pre')
-            //.position where we want it
-        .end()
-        //.do something else to $('#notes')
-    ;
-    $('.sticky-note-pre')
-        .draggable({
-            handle: '.handle'    
-        })
-        .resizable({
-            resize: function(){
-                var n = $(this);
-                n.find('.contents').css({
-                    width: n.width() - 40,
-                    height: n.height() - 60
-                });
-            }
-        })
-        .bind('click hover focus mousedown', function(){
-            $(this)
-                .css('zIndex', zIndex++)
-                .find('.ui-icon')
-                    .css('zIndex', zIndex++)
-                .end()
-            ;
-        })
-        .find('.close')
-            .click(function(){
-                $(this).parents('.sticky-note').remove();
-            })
-        .end()
-        .dblclick(function(){
-            $(this).remove();
-        })
-        .addClass('sticky-note')
-        .removeClass('sticky-note-pre')
-    ;
-});
+var notes, count = 0;
 
-$('#save').click(function(){
-    var notes = [], i, note;
-    $('.sticky-note').each(function(){
-        notes.push($(this).find('.contents').html());
+// save the notes into local storage
+function saveNotes() {
+    var notesArray = [];
+
+    // for each of the notes add a bespoke note object to the array
+    notes.find("li > div").each(function (i, e) {
+        // save the class attribute of the div, as well as the text for the title and content text areas
+        var colourClass = $(e).attr("class");
+        var title = $(e).find("textarea.note-title");
+        var content = $(e).find("textarea.note-content");
+
+        notesArray.push({ Index: i, Title: title.val(), Content: content.val(), Class: colourClass });
     });
-    //do something with notes
-    console.log(notes);
+
+    // json encode it
+    var jsonStr = JSON.stringify(notesArray);
+
+    // and save the json string into local storage
+    localStorage.setItem("notes", jsonStr);
+}
+
+// add event handlers to a note
+function addNoteEvent(noteElement) {
+    var div = noteElement.children("div");
+    var closeImg = div.find("img");
+
+    div.focus(function () {
+        closeImg.removeClass("hide");
+    });
+
+    div.children().focus(function () {
+        closeImg.removeClass("hide");
+    });
+
+    div.hover(function () {
+        closeImg.removeClass("hide");
+    }, function () {
+        closeImg.addClass("hide");
+        saveNotes();
+    });
+
+    div.children().hover(function () {
+        closeImg.removeClass("hide");
+    }, function () {
+        closeImg.addClass("hide");
+    });
+}
+			
+//  adds a new note to the 'notes' list
+function addNewNote(className, title, content) {
+	// if class is not specified, use a random colour class
+	if (!className) {
+		className = "colour";
+	}
+				
+	// add a new note to the end of the list
+	notes.append("<li><div class='" + className + "'>" + 
+					"<textarea class='note-title' placeholder='Untitled' maxlength='10'/>" + 
+					"<textarea class='note-content' placeholder='Your content here'/>" + 
+					"<img class='hide' src='images/close.png'/>" + 
+					"</div></li>");
+				
+	// get the new note that's just been added and attach the click event handler to its close button
+	var newNote = notes.find("li:last");
+	newNote.find("img").click(function () {
+        // remove the note and save
+	    newNote.remove();
+	    saveNotes();
+	});
+				
+	// hook up event handlers to show/hide close button as appropriate
+	addNoteEvent(newNote);
+				
+	// if a title is provided then set the title of the new note
+	if (title) {
+		// get the title textarea element and set its value
+		newNote.find("textarea.note-title").val(title);
+    }
+
+	// if a content is provided then set the content of the new note
+	if (content) {
+		// get the content textarea element and set its value
+		newNote.find("textarea.note-content").val(content);
+    }
+
+    // save
+    saveNotes();
+}
+
+// load the notes saved in the local storage
+function loadNotes() {
+    var storedNotes = localStorage.getItem("notes");
+    if (storedNotes) {
+        // passes the stored json back into an array of note objects
+        var notesArray = JSON.parse(storedNotes);
+        count = notesArray.length;
+
+        var i;
+        for (i = 0; i < count; i++) {
+            var storedNote = notesArray[i];
+            addNewNote(storedNote.Class, storedNote.Title, storedNote.Content);
+        }
+    }
+}
+
+$(document).ready(function () {
+    // get references to the 'notes' list
+    notes = $("#notes");
+
+    // load notes from local storage if one's available
+    loadNotes();
+
+    // clicking the 'New Note' button adds a new note to the list
+    $("#btnNew").click(function () {
+        addNewNote();
+    });
+
+    // add a note to the list if there aren't any
+    if (count === 0) {
+        $("#btnNew").click();
+    }
 });
